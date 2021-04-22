a63c28799f0b9c82fe0754631abdea622b2efcb2
guillaume dussart <guillaume@localdev.local>
2021-04-22 15:43:52 +0200

    first test project

---
+++ src/main/java/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,21 @@
+package fr.declaration.variable;
+
+
+public class DeclarationApp {
+
+
+    public static void main(String[] args) {
+        byte bytefirst = 10;
+        short shortfirst = -17282;
+        int intfirst = 28546952;
+        long longfirst = 1000L;
+        float floatfirst = 1.2f;
+        double doublefirst = 5.2d;
+        boolean boolfirst = true;
+        var total = bytefirst+intfirst;
+        String randonString = "Voici le résultat d’un calcul : \n"+bytefirst+"+"+intfirst+"="+total;
+        System.out.println(randonString);
+    }
+
+
+}
