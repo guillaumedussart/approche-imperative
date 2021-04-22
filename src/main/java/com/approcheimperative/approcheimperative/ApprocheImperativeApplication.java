a2352f91a05a295062599bc36021be4425b588bd
guillaume dussart <guillaume@localdev.local>
2021-04-22 13:48:15 +0200

    first commit

---
+++ src/main/java/com/approcheimperative/approcheimperative/ApprocheImperativeApplication.java
@@ -0,0 +1,13 @@
+package com.approcheimperative.approcheimperative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprocheImperativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApprocheImperativeApplication.class, args);
+    }
+
+}
