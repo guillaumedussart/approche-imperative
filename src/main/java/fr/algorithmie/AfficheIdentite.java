0daa64558355226be3af0290b65eeb2e9314ab29
guillaume dussart <guillaume@localdev.local>
2021-04-23 10:55:16 +0200

    finish ex 1

---
+++ src/main/java/fr/algorithmie/AfficheIdentite.java
@@ -0,0 +1,10 @@
+package fr.algorithmie;
+
+public class AfficheIdentite {
+    public static void main(String[] args) {
+        String indentity = "Guillaume Dussart";
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(indentity);
+        }
+    }
+}
