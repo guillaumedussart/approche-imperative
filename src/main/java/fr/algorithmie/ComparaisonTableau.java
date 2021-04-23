7aa4f1a4f67cc606cbfc73f25030f234fc24c56c
guillaume dussart <guillaume@localdev.local>
2021-04-23 14:27:16 +0200

    finish ex 12

--- src/main/java/fr/algorithmie/ComparaisonTableau.java
+++ src/main/java/fr/algorithmie/ComparaisonTableau.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class ComparaisonTableau {
 
-    public static void main(String[] args){
-        int[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, -4} ;
-        int[] array2 = {3, -8, 17, 5, -1, 4, 0, 6, 2, 11, -5, -4, 8} ;
+    public static void main(String[] args) {
+        int[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, -4};
+        int[] array2 = {3, -8, 17, 5, -1, 4, 0, 6, 2, 11, -5, -4, 8};
         int present = 0;
         for (int i = 0; i < array1.length; i++) {
 
@@ -18,6 +18,6 @@ public class ComparaisonTableau {
             }
 
         }
-        System.out.println("Il y a "+present+" elements identiques dans ces tableaux");
+        System.out.println("Il y a " + present + " elements identiques dans ces tableaux");
     }
 }
